4911e73954e44a4d82a16c702adf659335e27f3c
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-03-09 01:59:56 +0300

    CodeForces added

---
+++ CodeForce/HowMuchDoesDaytonaCost.py
@@ -0,0 +1,19 @@
+t = int(input())
+for i in range(t):
+    n, k = [int(i) for i in input().split()]
+    a = [int(i) for i in input().split()]
+    if k in a:
+        print("yes")
+    else:
+        print("no")
+
+
+
+
+
+
+
+
+
+
+
